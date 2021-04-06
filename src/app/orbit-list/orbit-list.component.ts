c32766b9de2c9c00c4d6b7a320ea56e93122807b
nlaleman <dragonmyace@gmail.com>
2021-04-06 12:36:30 -0500

    Final except Bonus

--- src/app/orbit-list/orbit-list.component.ts
+++ src/app/orbit-list/orbit-list.component.ts
@@ -1,12 +1,10 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Satellite } from '../satellite';
 
-@Component({
-  
+@Component({  
   selector: 'app-orbit-list',
   templateUrl: './orbit-list.component.html',
   styleUrls: ['./orbit-list.component.css']
-
 })
 
 
@@ -31,5 +29,19 @@ export class OrbitListComponent implements OnInit {
        return 0;
     });
  }
+
+//  search(searchTerm: string): void {
+//    let matchingSatellites: Satellite[] = [];
+//    searchTerm = searchTerm.toLowerCase();
+//    for(let j=0; j < this.sourceList.length; j++) {
+//       let name = this.sourceList[j].name.toLowerCase();
+//       if (name.indexOf(searchTerm) >= 0) {
+//          matchingSatellites.push(this.sourceList[j]);
+//       }
+//    }
+//    // assign this.displayList to be the array of matching satellites
+//    // this will cause Angular to re-make the table, but now only containing matches
+//    this.displayList = matchingSatellites;
+// }
 
 }
